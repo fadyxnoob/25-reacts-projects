727d3db808fbd3051f45305a63d0cb567d0c438e
Yasir Mehboob <115621928+fadyxnoob@users.noreply.github.com>
2024-06-16 10:44:26 +0500

    Add Accordians

--- accordians/src/App.jsx
+++ accordians/src/App.jsx
@@ -1,28 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import data from "./data";
 
-function App() {
+const App = () => {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSlection, setEnableMultiSlection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  // Creating single selection function
+  const handleSingleSelection = (gotCurrentID) => {
+    setSelected(gotCurrentID === selected ? null : gotCurrentID);
+  };
+  const handleMultiSelection = (gotCurrentID) => {
+    const cpyMultiple = [...multiple];
+    const findIndexOfCurrentID = cpyMultiple.indexOf(gotCurrentID);
+    if (findIndexOfCurrentID === -1) cpyMultiple.push(gotCurrentID);
+    else cpyMultiple.splice(findIndexOfCurrentID, 1);
+    setMultiple(cpyMultiple);
+  };
+  // console.log(selected, multiple);
   return (
     <div className="acc-wrapper">
-      <button >
-        Enable Multi Selection
-      </button>
       <div className="accordian">
-            <div className="item">
+        <h1 className="h1">React Accordian</h1>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
               <div
                 className="title"
+                onClick={
+                  enableMultiSlection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
               >
-                <h3></h3>
+                <h3>{dataItem.question}</h3>
                 <span>+</span>
-              </div>
-                    <div className="acc-content "></div>
-                    <div className="acc-content "></div>
-              
+               </div>
+              {
+                enableMultiSlection ? multiple.indexOf(dataItem.id) !== -1 && 
+                <div className="acc-content">{dataItem.answer}</div> : 
+                selected === dataItem.id && <div className="acc-content">{dataItem.answer}</div>
+              } 
             </div>
-        
+          ))
+        ) : (
+          <div>No Data Found</div>
+        )}
       </div>
+      <button onClick={() => setEnableMultiSlection(!enableMultiSlection)}>
+        {enableMultiSlection ? "Disable" : "Enable" } Multi Selection
+      </button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
